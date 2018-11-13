f7901d6fe54a8de41ece00b219b438107c598c30
henrikjohansson712 <henrik.johansson712@gmail.com>
2018-11-13 15:19:03 +0100

    added js for auto update of total

--- js/yatzytable.js
+++ js/yatzytable.js
@@ -16,24 +16,41 @@ let dice = [
   "Dice 6"
 ];
 
-let player1_ones = parseInt(document.getElementById("player1_ones").value);
+const BONUS = 75;
+
+const player1_ones = document.getElementById("player1_ones");
+const player1_twos = document.getElementById("player1_twos");
+const player1_threes = document.getElementById("player1_threes");
+const player1_fours = document.getElementById("player1_fours");
+const player1_fives = document.getElementById("player1_fives");
+const player1_sixes = document.getElementById("player1_sixes");
 
 function updateTotal1() {
   let scores = [];
 
-  scores.push(parseInt(document.getElementById("player1_ones").value));
-  scores.push(parseInt(document.getElementById("player1_twos").value));
-  scores.push(parseInt(document.getElementById("player1_threes").value));
-  scores.push(parseInt(document.getElementById("player1_fours").value));
-  scores.push(parseInt(document.getElementById("player1_fives").value));
-  scores.push(parseInt(document.getElementById("player1_sixes").value));
+  scores.push(parseInt(player1_ones.value));
+  scores.push(parseInt(player1_twos.value));
+  scores.push(parseInt(player1_threes.value));
+  scores.push(parseInt(player1_fours.value));
+  scores.push(parseInt(player1_fives.value));
+  scores.push(parseInt(player1_sixes.value));
 
   let total1 = 0;
   var i;
   for (i = 0; i < scores.length; i++) {
     total1 += scores[i];
   }
-  return document.getElementById("player1_total1").innerHTML = total1;
+  document.getElementById("player1_total1").innerHTML = total1;
+  if (total1 >= 63) {
+    document.getElementById("player1_bonus").innerHTML = BONUS;
+  }
 }
 
-player1_ones.addEventListener("oninput", updateTotal1);
+
+player1_ones.addEventListener("change", updateTotal1);
+player1_twos.addEventListener("change", updateTotal1);
+player1_threes.addEventListener("change", updateTotal1);
+player1_fours.addEventListener("change", updateTotal1);
+player1_fives.addEventListener("change", updateTotal1);
+player1_sixes.addEventListener("change", updateTotal1);
+
