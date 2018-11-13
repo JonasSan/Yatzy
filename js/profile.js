0e4341b95dbe4ee6f1bc65052b95ff84bb5fb7c3
welket <welid.ketab@gmail.com>
2018-11-13 11:05:28 +0100

    Javascriptfil för Profilesidan.

---
+++ js/profile.js
@@ -0,0 +1,14 @@
+användar_lista = [];
+
+
+
+let användare = {
+   namn: "Henrik",
+   land: "Sverige",
+   spelade_partier: 2,
+   partie_poäng: 160,
+   högsta_poäng: 130,
+   lägsta_poäng: 70,
+};
+
+användar_lista.push(användare);
