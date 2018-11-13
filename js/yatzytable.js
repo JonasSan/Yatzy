f6ec3c835f96c020084e1de3c149dd251e98cfcf
henrikjohansson712 <henrik.johansson712@gmail.com>
2018-11-13 10:57:33 +0100

    Added js file for yatzytable

---
+++ js/yatzytable.js
@@ -0,0 +1,17 @@
+let game = [];
+
+let player = {
+  user: "user",
+  score: "0"
+};
+
+game.push(player);
+
+let dice = [
+  "Dice 1",
+  "Dice 2",
+  "Dice 3",
+  "Dice 4",
+  "Dice 5",
+  "Dice 6"
+];
