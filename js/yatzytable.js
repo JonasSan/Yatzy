34a4816eefc1b3a015b1b0a58567f2a6ac819d8c
henrikjohansson712 <henrik.johansson712@gmail.com>
2018-11-14 09:22:48 +0100

    added new js function for score calculation

--- js/yatzytable.js
+++ js/yatzytable.js
@@ -1,56 +1,78 @@
-let game = [];
+document.addEventListener("DOMContentLoaded", function (event) {
 
-let player = {
-  user: "user",
-  score: "0"
-};
+  let game = [];
 
-game.push(player);
+  let player = {
+    user: "user",
+    score: "0"
+  };
 
-let dice = [
-  "Dice 1",
-  "Dice 2",
-  "Dice 3",
-  "Dice 4",
-  "Dice 5",
-  "Dice 6"
-];
+  game.push(player);
 
-const BONUS = 75;
+  let dice = [
+    "Dice 1",
+    "Dice 2",
+    "Dice 3",
+    "Dice 4",
+    "Dice 5",
+    "Dice 6"
+  ];
 
-const player1_ones = document.getElementById("player1_ones");
-const player1_twos = document.getElementById("player1_twos");
-const player1_threes = document.getElementById("player1_threes");
-const player1_fours = document.getElementById("player1_fours");
-const player1_fives = document.getElementById("player1_fives");
-const player1_sixes = document.getElementById("player1_sixes");
+  const BONUS = 75;
 
-function updateTotal1() {
-  let scores = [];
+  const player1_ones = document.getElementById("player1_ones");
+  const player1_twos = document.getElementById("player1_twos");
+  const player1_threes = document.getElementById("player1_threes");
+  const player1_fours = document.getElementById("player1_fours");
+  const player1_fives = document.getElementById("player1_fives");
+  const player1_sixes = document.getElementById("player1_sixes");
 
-  scores.push(parseInt(player1_ones.value));
-  scores.push(parseInt(player1_twos.value));
-  scores.push(parseInt(player1_threes.value));
-  scores.push(parseInt(player1_fours.value));
-  scores.push(parseInt(player1_fives.value));
-  scores.push(parseInt(player1_sixes.value));
+  function updateTotal1() {
+    let scores = [];
 
-  let total1 = 0;
-  var i;
-  for (i = 0; i < scores.length; i++) {
-    total1 += scores[i];
+    scores.push(parseInt(player1_ones.value));
+    scores.push(parseInt(player1_twos.value));
+    scores.push(parseInt(player1_threes.value));
+    scores.push(parseInt(player1_fours.value));
+    scores.push(parseInt(player1_fives.value));
+    scores.push(parseInt(player1_sixes.value));
+
+    let total1 = 0;
+    var i;
+    for (i = 0; i < scores.length; i++) {
+      total1 += scores[i];
+    }
+    document.getElementById("player1_total1").innerHTML = total1;
+    if (total1 >= 63) {
+      document.getElementById("player1_bonus").innerHTML = BONUS;
+    } else {
+      document.getElementById("player1_bonus").innerHTML = 0;
+    }
   }
-  document.getElementById("player1_total1").innerHTML = total1;
-  if (total1 >= 63) {
-    document.getElementById("player1_bonus").innerHTML = BONUS;
+
+
+  player1_ones.addEventListener("change", updateTotal1);
+  player1_twos.addEventListener("change", updateTotal1);
+  player1_threes.addEventListener("change", updateTotal1);
+  player1_fours.addEventListener("change", updateTotal1);
+  player1_fives.addEventListener("change", updateTotal1);
+  player1_sixes.addEventListener("change", updateTotal1);
+
+
+  var savedDices = [];
+
+  function getDiceNumber() {
+    for (let i = 1; i <= 6; i++) {
+      return document.getElementById("dice" + i).innerHTML = Math.floor(Math.random() * (7 - 1) + 1);
+    }
+    //console.log(savedDices);
   }
-}
+
+  console.log(savedDices);
 
 
-player1_ones.addEventListener("change", updateTotal1);
-player1_twos.addEventListener("change", updateTotal1);
-player1_threes.addEventListener("change", updateTotal1);
-player1_fours.addEventListener("change", updateTotal1);
-player1_fives.addEventListener("change", updateTotal1);
-player1_sixes.addEventListener("change", updateTotal1);
+  let roll = document.getElementById("roll");
 
+  roll.addEventListener("click", getDiceNumber);
+
+});
